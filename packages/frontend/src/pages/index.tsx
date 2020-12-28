b5e155f95f075b11c587e496d7c68ad37ee41b88
sugoyuki <sugoyuki43@gmail.com>
2020-12-28 17:57:33 +0900

    refactored

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,9 +1,31 @@
 import React, { useState } from "react";
 import { NextPage } from "next";
-import Link from "next/link";
 import styled from "styled-components";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+
+type RGBSliderProps = {
+  value: number;
+  onChange: (value: number) => any;
+};
+
+const RGBSlider: React.FC<RGBSliderProps> = ({ value, onChange }) => {
+  return (
+    <Slider
+      className="col-span-1 mx-3"
+      min={0}
+      max={255}
+      onChange={(value: number) => {
+        onChange(value);
+      }}
+      defaultValue={255}
+      startPoint={0}
+      value={value}
+      vertical
+      reverse
+    />
+  );
+};
 
 const RootPage: NextPage = () => {
   const [red, setRed] = useState(0);
@@ -22,50 +44,25 @@ const RootPage: NextPage = () => {
   };
 
   return (
-    <BgDiv red={red} green={green} blue={blue}>
-      <p className="text-center">{rgbToHex(red, green, blue)}</p>
-      <div className="grid grid-cols-3 gap-1 h-80 items-center justify-items-center">
-        <Slider
-          className="col-span-1 mx-3"
-          min={0}
-          max={255}
-          onChange={(value: number) => {
-            setRed(value);
-          }}
-          defaultValue={255}
-          startPoint={0}
-          value={red}
-          vertical
-          reverse
-        />
-        <Slider
-          className="col-span-1 mx-3"
-          min={0}
-          max={255}
-          onChange={(value: number) => {
-            setGreen(value);
-          }}
-          defaultValue={255}
-          startPoint={0}
-          value={green}
-          vertical
-          reverse
-        />
-        <Slider
-          className="col-span-1 mx-3"
-          min={0}
-          max={255}
-          onChange={(value: number) => {
-            setBlue(value);
-          }}
-          defaultValue={255}
-          startPoint={0}
-          value={blue}
-          vertical
-          reverse
-        />
-      </div>
-    </BgDiv>
+    <>
+      <BgDiv red={red} green={green} blue={blue}>
+        <div className="block mx-auto max-w-xl">
+          <p className="py-8 text-center text-xl">{rgbToHex(red, green, blue)}</p>
+          <div className="grid grid-cols-3 gap-1 h-80 items-center justify-items-center">
+            <RGBSlider value={red} onChange={(value: number) => setRed(value)} />
+            <RGBSlider value={green} onChange={(value: number) => setGreen(value)} />
+            <RGBSlider value={blue} onChange={(value: number) => setBlue(value)} />
+          </div>
+        </div>
+      </BgDiv>
+      <footer className="bg-white">
+        <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+          <p className="mt-8 text-center text-base text-gray-400">
+            &copy; 2020 Team Nagatacho. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    </>
   );
 };
 
